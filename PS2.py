a9f705976967de82ba6512616e7116f2c834d94d
chpmoreno <chpmoreno@gmail.com>
2017-05-11 02:48:19 +0200

    more than one topic and samples storing

--- PS2.py
+++ PS2.py
@@ -16,18 +16,18 @@ import time
 ### GIBBS SAMPLER
 
 
-path = "/Users/b.yc0006/Cloud/term3/textmining/"
+path = "/home/chpmoreno/Dropbox/Documents/BGSE/Third_Term/TMSC/homeworks/github/Text-Mining/hw1/"
 data = pd.read_table(path+"speech_data_extend.txt",encoding="utf-8")
-data = data.loc[data['year']>=1990]
+data = data.loc[data['year']>=1946]
 data = data.reset_index()
 
 
 def data_preparation(data):
     prep_data = data.apply(lambda row: #tokenize
-                            word_tokenize(row['speech'].lower()), axis=1)    
+                            word_tokenize(row['speech'].lower()), axis=1)
     stop_w=set(stopwords.words('english')) #stopwords
     for i in range(len(prep_data)): #non-alphanumeric characters
-        prep_data[i] = [w for w in prep_data[i] if w not in stop_w and w.isalpha()] 
+        prep_data[i] = [w for w in prep_data[i] if w not in stop_w and w.isalpha()]
     stemmer = PorterStemmer() #Create a stemmer object
     for i in range(len(prep_data)): #Stem the data
         prep_data[i] = [stemmer.stem(elem) for elem in prep_data[i]]
@@ -37,31 +37,29 @@ def data_preparation(data):
 
 prep_data, unique_words = data_preparation(data)
 
-D = len(prep_data) #Number of documents
-K = 2 #Number of topics
-V = len(unique_words)#Number of unique terms
+#theta = dirichlet([alpha]*K,D)
+#beta = dirichlet([eta]*K,V)
+
+# Problem: generate Z_dn, a list of dimension d of lists, each one of different length
+# (number of terms of each document) with values in k. So: topic allocation of word
+# n in document d. To assign a new topic to each entry, we need to match term v
+# in Beta_v (which will be stored in "unique_words") with word n in Z_dn (which
+# will be stored in "prep_data").
+
+
+### Auxiliar functions Z sample
 def simulate(K, row):
     samples = np.random.multinomial(1,[1/K]*K,len(prep_data[row])).tolist()
     samples_correct = []
     for s in samples:
         samples_correct.append(s.index(1))
     return samples_correct
-Z = prep_data.apply(lambda row: simulate(K,row)) #Z_dn        
-N = np.zeros((D,K))
-M = np.zeros((K,V))
 
-#Initial values (I took Euan's, no idea how to set them)
-alpha = 50/K
-eta = 200/V
-
-theta = dirichlet([alpha]*K,D)
-beta = dirichlet([eta]*K,V)
-
-# Problem: generate Z_dn, a list of dimension d of lists, each one of different length
-# (number of terms of each document) with values in k. So: topic allocation of word
-# n in document d. To assign a new topic to each entry, we need to match term v 
-# in Beta_v (which will be stored in "unique_words") with word n in Z_dn (which 
-# will be stored in "prep_data").
+def N_count(Z_d, K):
+    N_count_vector = []
+    for k in range(K):
+        N_count_vector.append(Z_d.count(k))
+    return N_count_vector
 
 ### Sample for topic allocation
 def sample_topic(Z, theta, beta):
@@ -76,16 +74,14 @@ def sample_topic(Z, theta, beta):
 #sample_topic(Z, theta, beta)
 
 ### Sample for theta
-
 def sample_theta(Z,alpha,theta):
     D,K = theta.shape
     N = np.zeros((D,K))
     for d in range(D):
-        N[d,:] = np.unique(Z[d], return_counts=True)[1]
+        #N[d,:] = np.unique(Z[d], return_counts=True)[1]
+        N[d,:] = N_count(Z[d], K)
         theta[d,:] = dirichlet(N[d,:] + alpha)
     return theta
-
-#theta = sample_theta(Z,alpha,theta)
 
 ### Sample for beta
 
@@ -107,21 +103,37 @@ def sample_beta(Z,prep_data,eta,beta):
 
 #beta = sample_beta(Z,prep_data,eta,beta)
 
-
 def gibbs_sampler(n_iter,prep_data,alpha,eta,K):
     ## Initialize objects
     D = len(prep_data)
     theta = dirichlet([alpha]*K,D)
     beta = dirichlet([eta]*K,V)
     Z = prep_data.apply(lambda row: simulate(K,row))
+    Z_dist = []
+    theta_dist = []
+    beta_dist = []
     for i in range(n_iter):
         print('Iteration nº:'+ str(i))
         start = time.time()
         Z = sample_topic(Z,theta,beta)
         theta = sample_theta(Z,alpha,theta)
         beta = sample_beta(Z,prep_data,eta,beta)
+        Z_dist.append(Z)
+        theta_dist.append(theta)
+        beta_dist.append(beta)
         print('Duration:'+ str(time.time()-start))
-    return Z,theta,beta
+    return Z_dist, theta_dist, beta_dist
 
+### Initial parameters
+#D = len(prep_data) #Number of documents
+#V = len(unique_words)#Number of unique terms
+#Z = prep_data.apply(lambda row: simulate(K,row)) #Z_dn
+#N = np.zeros((D,K))
+#M = np.zeros((K,V))
 
-Z,theta,beta = gibbs_sampler(10,prep_data,alpha,eta,3)
+#Initial values (reference original paper)
+K = 10 #Number of topics
+alpha = 50/K
+eta = 200/V
+
+Z_2, theta_2, beta_2 = gibbs_sampler(5000,prep_data, alpha, eta, K)
